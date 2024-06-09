d730a2dc81068f09b2d5a0142842ef9155368565
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2024-06-09 17:27:17 +0100

    Add native libs

---
+++ script-core/ExamplePlugin/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Plugify 
+{
+	class Plugin 
+	{
+		public static int OnInit()
+		{
+			// This function is executed the first time the Plugin is init
+
+			return 0;
+		}
+		
+		public static void OnStart()
+		{
+			// This function is executed the first time the Plugin is started
+		}
+		
+		public static void OnEnd()
+		{
+			// This function is executed the first time the Plugin is ended
+		}
+	}
+}
---
+++ script-core/Plugify/Library.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+
+namespace Plugify;
+
+public static class Library
+{
+    [UnmanagedCalleUNrsOnly]
+    public static void Initialize()
+    {
+        // TODO: Implement basic staff
+    }
+}
